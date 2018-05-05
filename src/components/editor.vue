5831f35af720d5d69112acaaaf38d6fae06055dd
coder board <1019463979@qq.com>
2018-05-05 11:39:59 +0800

    5.5更新，清除及纠错更新

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -1,12 +1,21 @@
 <template>
 	<div class="content">
 		<div class="main">
-		<div id="editor" style="text-align: left;" class="view"></div>
-		<div class="correct-area">
-			<div id="banner"><img src="../assets/logo1.png"></div>
-			<textarea id="correct"></textarea>
+			<div id="editor" style="text-align: left;" class="view"></div>
+			<div class="correct-area">
+				<div id="banner"><img src="../assets/logo1.png"></div>
+				<textarea id="correct" readonly="readonly"></textarea>
+			</div>
 		</div>
-	</div>
+		<div class="correct-bar">
+			<select id="language">
+				<option>简体中文</option>
+				<option>英文</option>
+				<option>印尼语</option>
+			</select>
+			<button class="correct" id="correctClick">一键纠错</button>
+			<button class="clear" id="clear">清除内容</button>
+		</div>
 	</div>
 </template>
 
@@ -24,7 +33,6 @@
 			getContent:function(){
 				alert(this.editorContent);
 			}
-
 		},
 		mounted(){
 			var editor = new E('#editor');
@@ -33,12 +41,27 @@
 					type:'POST',
 					url:'',
 					data:{},
-					dataType:,
+					dataType:"json",
 					success:function(data){
 						$('#correct').text(data);
 					}
-				})
+				});
+			};
+			editor.customConfig.onchangeTimeout = 1500;
+			editor.customConfig.pasteFilterStyle = false;
+			editor.customConfig.pasteIgnoreImg = true;
+			editor.customConfig.pasteTextHandle = function (content) {
+        		return content;
+    		}
 			editor.create();
+
+			document.getElementById('clear').addEventListener('click',function(){
+				editor.txt.clear();
+			});
+
+			document.getElementById('correctClick').addEventListener('click',function(){
+
+			})
 		}
 	}
 </script>
@@ -59,7 +82,7 @@
  		margin-top: -50px;
  	}
 	.view{
-		flex: 2;
+		flex: 1;
 		z-index:0;
 		background-color: white;
 	}
@@ -90,4 +113,38 @@
 		align-self: flex-end;
 	}
 
+	.correct-bar{
+		float: left;
+		width: 50%;
+		margin-left: -39%;
+	}
+
+	.correct-bar .clear , .correct{
+		float: right;
+		display: inline-block;
+		width: 80px;
+		height: 38px;
+		margin-left:20px;
+		margin-top: 5px;
+		font-size: 15px;
+		line-height: 35px; 
+		background-color: #11A586;
+		text-align: center;
+		border-radius: 5px;
+		cursor: pointer;
+		outline: none;
+		color: white;
+		border: 1px;
+	}
+
+	#language{
+		width: 90px;
+		height: 38px;
+		margin-left:-8px;
+		margin-top: 5px;
+		font-size: 15px;
+		line-height: 35px; 
+		outline: none;
+		border-radius: 5px;
+	}
 </style>
